c42dd735fb2b31d199d71c5ac40d007506e1042e
Pedro Mendoza <lpedromendoza333@gmail.com>
2023-03-31 18:03:00 -0600

    Graphql init project

---
+++ index.js
@@ -0,0 +1,59 @@
+import { gql, ApolloServer } from "apollo-server";
+
+const persons = [
+  { 
+    name: 'John',
+    age: 20, 
+    phone: '123-456-7890', 
+    street: '123 Main St', 
+    city: 'New York',
+    id: '1' },
+]
+
+const typeDefs = gql`
+  type Address { 
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person]!
+    findPerson(name: String!): Person
+  }
+`;
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => {
+      const { name } = args;
+      return persons.find(p => p.name === name);
+    },
+  },
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
