c8667eb672baebb1716225fb08d029e1ce93f067
Pedro Mendoza <lpedromendoza333@gmail.com>
2023-04-03 16:20:27 -0600

    Deleting node modules

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { gql, ApolloServer } from "apollo-server";
+import { gql, ApolloServer, UserInputError } from "apollo-server";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 const persons = [
   { 
@@ -7,10 +9,22 @@ const persons = [
     phone: '123-456-7890', 
     street: '123 Main St', 
     city: 'New York',
-    id: '1' },
+    id: '1' 
+  },
+  { 
+    name: 'Peter',
+    age: 20, 
+    street: '123 Main St', 
+    city: 'New York',
+    id: '2' 
+  },
 ]
 
 const typeDefs = gql`
+  enum YesNo {
+    YES
+    NO
+  }
   type Address { 
     street: String!
     city: String!
@@ -25,19 +39,61 @@ const typeDefs = gql`
 
   type Query {
     personCount: Int!
-    allPersons: [Person]!
+    allPersons(phone: YesNo): [Person]!
     findPerson(name: String!): Person
+  }
+  
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
   }
 `;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: async (root, args) => {
+      const { data: personsFromAPI } = await axios('http://localhost:3001/persons');
+      if (!args.phone) return personsFromAPI; 
+
+      const byPhone = person => args.phone === 'YES' ? person.phone : !person.phone;
+
+      return personsFromAPI.filter(byPhone);
+    },
     findPerson: (root, args) => {
       const { name } = args;
       return persons.find(p => p.name === name);
     },
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      const person = { ...args, id: uuidv4() }
+      if (!persons.find(p => p.name === person.name)) {
+        persons.push(person);
+      } else {
+        // Error handling with graphql
+        throw new UserInputError('Name must be unique', {
+          invalidArgs: args.name
+        });
+      }
+      return person;
+    },
+    editNumber: (root, args) => {
+      const personIndex = persons.findIndex(p => p.name === args.name);
+      if (personIndex === -1) return null;
+      const person = persons[personIndex];
+      const updatedPerson = { ...person, phone: args.phone };
+      persons[personIndex] = updatedPerson;
+      return updatedPerson;
+    }
   },
   Person: {
     address: (root) => {
